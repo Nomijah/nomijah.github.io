f55ec8d2ed34e0d29866a5ee0df30893124f873e
Petter Boström <petter.bostrom@gmail.com>
2023-03-27 13:46:01 +0200

    Fixed problem 1337 easter egg working only once

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,7 @@ fetch("../JSON/WorkStudy.json")
     }
   });
 
+// Loading content for study page
 const studyDisplay = (array) => {
   array.forEach((e) => {
     let newDiv = document.createElement("div");
@@ -35,6 +36,7 @@ const studyDisplay = (array) => {
   removeLoading();
 };
 
+// Loading content for work page
 const workDisplay = (array) => {
   array.forEach((e) => {
     let newDiv = document.createElement("div");
@@ -53,12 +55,14 @@ const workDisplay = (array) => {
   removeLoading();
 };
 
+// Function to remove the loading indicator
 const removeLoading = () => {
   document
     .querySelector("main")
     .removeChild(document.querySelector(".loading"));
 };
 
+// Secret click easter egg
 const homeFuncs = () => {
   let buttonState = false;
   document.getElementById("thebutton").onclick = (evt) => {
@@ -70,4 +74,32 @@ const homeFuncs = () => {
       buttonState = false;
     }
   };
+
+  // 1337 Easter egg
+  let modal = document.getElementById("myModal");
+  let counter = 0;
+  let keyPressOrder = [1, 3, 3, 7];
+
+  const removeModal = () => {
+    modal.style.display = "none";
+    counter = 0;
+    document.removeEventListener("keydown", removeModal);
+  };
+
+  // Event listener for 1337 easter egg
+  document.addEventListener("keydown", ({ key }) => {
+    if (key == keyPressOrder[counter]) {
+      console.log("hej från knapptryck");
+      counter++;
+      console.log(counter);
+    } else {
+      counter = 0;
+    }
+    if (counter === 4) {
+      console.log("lyckas");
+      modal.style.display = "block";
+      document.addEventListener("keydown", removeModal);
+    }
+  });
+  console.log(counter);
 };
